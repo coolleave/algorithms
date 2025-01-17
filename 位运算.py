ce2be58e83bc374e05c8e214e300a70a53930807
coolleave <cpy2855048477@163.com>
2025-01-17 12:49:33 +0800

    位运算去重

--- 位运算.py
+++ 位运算.py
@@ -1,4 +1,8 @@
+
 # 利用位运算判断是否为奇偶数
+import random
+
+
 def isObb(n):
     if n & 1:
         print("obb")
@@ -6,5 +10,38 @@ def isObb(n):
         print("even")
 
 
+# 利用异或去重
+"""
+异或 ^   
+1^0 = 1; 0^1 = 1; 1^1 = 0; 0^0 = 0;
+利用异或可以去重
+思路 有n+1个不同的数,在1到n范围内,找出重复一次的数字x0
+异或去重 x^x = 0; x^0 = x
+(x1^x2^...^xn+1)^(1^2^...^n) = x0
+
+"""
+
+
+def dr(n):
+
+    # 生成值为1到n的数组
+    arr = list(range(1, n + 1))
+    # 随机选择一个值插入到数组中，使数组有n+1个元素
+    random_value = random.choice(arr)  # 生成随机数
+    arr.append(random_value)  # 将随机数添加到数组末尾
+    # 随机打乱数组顺序
+    random.shuffle(arr)
+    print(arr)
+    print(random_value)
+    x0 = 0
+    for x in range(n+1):
+        x0 = x0 ^ x  # 1^2^...^n
+    for x in arr:
+        x0 = x0 ^ x  # (x1^x2^...^xn+1)^(1^2^...^n)
+
+    print(x0)
+
+
 if __name__ == '__main__':
-    isObb(3)
+    # isObb(3)
+    dr(1000)
