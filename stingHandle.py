644031198c0276c5e3702946eb95810fa221ec2a
coolleave <cpy2855048477@163.com>
2025-01-25 18:33:54 +0800

    处理字符串

---
+++ stingHandle.py
@@ -0,0 +1,36 @@
+"""
+问题描述
+给定两个仅由大写字母或小写字母组成的字符串（长度介于1到10之间），它们之间的关系是以下4中情况之一：
+1:两个字符串长度不等。比如Beijing和Hebei
+2:两个字符串不仅长度相等，而目相应位置上的字符完全一致（区分大小写），比如Beijing和Beijing
+3:两个字符串长度相等，相应位置上的字符仅在不区分大小写的前提下才能达到完全一致（也就是说，它并不满足情况2）。比如
+beijing和BEljing
+4:两个字符串长度相等，但是即使是不区分大小写也不能使这两个字符串一致。比如Beijing和Nanjing
+编程判断输入的两个字符串之间的关烈属于这四类中的哪一类，给出所属的类的编号。
+输入格式
+包括两行，每行都是一个字符串
+输出格式
+仅有一个数字，表明这两个字符串的关系编号
+样例输入
+BEljing
+beiJing
+样例输出
+3
+"""
+str1, str2 = input().split()
+
+
+def stringHandle(str1, str2):
+    if len(str1) != len(str2):
+        return 1
+    else:
+        if str1.lower() != str2.lower():
+            return 4
+        else:
+            if str1 == str2:
+                return 2  # 完全相等
+            else:
+                return 3
+
+
+print(stringHandle(str1, str2))
